260970d491fa84bb94c5dd3242d5124b043f8143
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-02-15 15:22:29 +0500

    changes made

---
+++ design/app.js
@@ -0,0 +1,7 @@
+const slider = document.getElementById("silder");
+const price = document.getElementById("price");
+const package = document.getElementById("package");
+
+slider.oninput = () => {
+    price.innerText = 'haha';
+}
