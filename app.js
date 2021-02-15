1ceaaa79a81fe0dd12e59572435c9e8ff683093a
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-02-15 15:48:40 +0500

    changes made

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const slider = document.getElementById("silder");
+const slider = document.getElementById("slider");
 const price = document.getElementById("price");
 const package = document.getElementById("package");
 
 slider.oninput = () => {
-    console.log("haha");
+    switch(slider.value) {
+        case "1":
+            price.innerText = "8.00";
+            package.innerText = "10K";
+            break;
+        case "2":
+            price.innerText = "12.00"
+            package.innerText = "50K";
+            break;
+        case "3":
+            price.innerText = "16.00";
+            package.innerText = "100K";
+            break;
+        case "4":
+            price.innerText = "24.00";
+            package.innerText = "500K";
+            break;
+        case "5":
+            price.innerText = "36.00";
+            package.innerText = "1M";
+            break;
+    }  
 }
