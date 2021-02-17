335b429182daef04a52ca79299404624ba433a02
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-02-17 19:32:30 +0500

    changes made

--- app.js
+++ app.js
@@ -9,47 +9,45 @@ let num = 16;
 let yearly = 144;
 
 slider.oninput = () => {
-    switch(slider.value) {
-        case "1":
-            num = 8;
-            package.innerText = "10K";
-            fill.style.width = "0";
-            break;
-        case "2":
-            num = 12;
-            package.innerText = "50K";
-            fill.style.width = "25%";
-            break;
-        case "3":
-            num = 16;
-            package.innerText = "100K";
-            fill.style.width = "50%";
-            break;
-        case "4":
-            num = 24;
-            package.innerText = "500K";
-            fill.style.width = "75%";
-            break;
-        case "5":
-            num = 36;
-            package.innerText = "1M";
-            fill.style.width = "100%";
-            break;
-    }  
+  switch (slider.value) {
+    case "1":
+      num = 8;
+      package.innerText = "10K";
+      fill.style.width = "0";
+      break;
+    case "2":
+      num = 12;
+      package.innerText = "50K";
+      fill.style.width = "25%";
+      break;
+    case "3":
+      num = 16;
+      package.innerText = "100K";
+      fill.style.width = "50%";
+      break;
+    case "4":
+      num = 24;
+      package.innerText = "500K";
+      fill.style.width = "75%";
+      break;
+    case "5":
+      num = 36;
+      package.innerText = "1M";
+      fill.style.width = "100%";
+      break;
+  }
 
-    yearly = ((num * 12) - (((num * 12)* 25) / 100)).toFixed(0);
-    
-    
-    price.innerText = toggle.checked ? "$" + yearly + ".00" : "$" + num + ".00";
-}
+  yearly = (num * 12 - (num * 12 * 25) / 100).toFixed(0);
+
+  price.innerText = toggle.checked ? "$" + yearly + ".00" : "$" + num + ".00";
+};
 
 toggle.oninput = () => {
-    if (toggle.checked) {
-        plan.innerText = "/ year";
-        price.innerText = "$" + yearly + ".00";
-    }
-    else {
-        plan.innerText = "/ month";
-        price.innerText = "$" + num + ".00";
-    }
-}
+  if (toggle.checked) {
+    plan.innerText = "/ year";
+    price.innerText = "$" + yearly + ".00";
+  } else {
+    plan.innerText = "/ month";
+    price.innerText = "$" + num + ".00";
+  }
+};
